4a72663c994519a1811166b9b6f6a35cc07195bc
root <nine4567@gmail.com>
2019-05-22 13:43:45 +0700

    edit view fix bug input

--- test/main.test.js
+++ test/main.test.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 const Logo = require('../helpers/logo');
 mongoose.Promise = global.Promise;
 let Collection = 'exammanage_test';
-const config = require('./configuration/app.json');
+const config = require('../configuration/app.json');
 mongoose.connect('mongodb://'+config.DB_HOST+':'+config.DB_PORT+'/'+Collection, { useNewUrlParser: true });
 mongoose.set('useCreateIndex', true);
 require('../helpers/general');
